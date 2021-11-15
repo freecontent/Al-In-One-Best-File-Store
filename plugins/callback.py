36a96e492e21eb2c88504ce8efbc40b1a99a05c4
freecontent <84922944+freecontent@users.noreply.github.com>
2021-11-15 13:43:48 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -71,8 +71,6 @@ async def about_cb(c, m):
 👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: {owner.mention(style='md')}
 
 📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [FREE BOT UPDATES](https://t.me/TG_FreeBots_Support)
-
-🌐 𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](https://github.com/freecontent/My-File-Store-V2-Bot)
 """
 
     # creating buttons
